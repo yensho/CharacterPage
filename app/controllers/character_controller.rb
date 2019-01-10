83e595e71ab179bc866bfc2c921c765476daea11
Stewart Font <yensho@gmail.com>
2019-01-10 00:11:55 -0600

    Built show view, new is added but not working.

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -7,7 +7,24 @@ class CharacterController < ApplicationController
   def new
   end
 
-  def create
+  def show
+    @character = Character.find(params[:id])
   end
 
+  def create
+    @character = Character.new(character_params)
+
+    if @character.save
+      redirect_to @character
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def character_params
+    params.require(:character).permit(:character)
+  end
 end
